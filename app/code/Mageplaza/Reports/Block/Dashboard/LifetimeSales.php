5d630b057392819816b4874ffea5598e658af019
atniqi <atniqii@gmail.com>
2019-10-26 16:40:28 +0200

    m

---
+++ app/code/Mageplaza/Reports/Block/Dashboard/LifetimeSales.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Reports
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\Reports\Block\Dashboard;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Phrase;
+
+/**
+ * Class LifetimeSales
+ * @package Mageplaza\Reports\Block\Dashboard
+ */
+class LifetimeSales extends AbstractClass
+{
+    const NAME = 'lifetimeSales';
+
+    /**
+     * @return Phrase|string
+     */
+    public function getTitle()
+    {
+        return __('Lifetime Sales');
+    }
+
+    /**
+     * @return string
+     * @throws LocalizedException
+     */
+    public function getTotal()
+    {
+        $total = $this->_helperData->getLifetimeSales();
+        if (isset($total['lifetime'])) {
+            return $this->format($total['lifetime']);
+        }
+
+        return '';
+    }
+}
